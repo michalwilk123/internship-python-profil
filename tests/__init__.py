374ad53380d6924765fc7c1b4eb2441b2d9cb7a0
michal <michal.wilk0@yahoo.com>
2021-07-05 21:51:26 +0200

    created absic unittest for ProfilLogger

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+from .test_profil_logger import TestProfilLogger
---
+++ tests/test_profil_logger.py
@@ -0,0 +1,33 @@
+from my_logger.profil_logger import ProfilLogger
+import unittest
+
+
+class TestProfilLogger(unittest.TestCase):
+    def setUp(self) -> None:
+        self.profil_logger = ProfilLogger(handlers=[])
+
+    def test_log_level(self):
+        self.assertTrue(self.profil_logger.debug("Test Log"))
+        self.assertTrue(self.profil_logger.info("Test Log"))
+        self.assertTrue(self.profil_logger.warning("Test Log"))
+        self.assertTrue(self.profil_logger.error("Test Log"))
+        self.assertTrue(self.profil_logger.critical("Test Log"))
+
+    def test_min_log_change(self):
+        self.profil_logger.set_log_level('WARNING')
+
+        self.assertFalse(self.profil_logger.debug("Test Log"))
+        self.assertFalse(self.profil_logger.info("Test Log"))
+        self.assertTrue(self.profil_logger.warning("Test Log"))
+        self.assertTrue(self.profil_logger.error("Test Log"))
+        self.assertTrue(self.profil_logger.critical("Test Log"))
+
+    def test_bad_level_name(self):
+        self.assertRaises(
+            KeyError, 
+            self.profil_logger.set_log_level,
+            "bad level name"
+        )
+        
+        
+
