cc748866d6a29f302b4231a87576b9f454bd41d2
michal <michal.wilk0@yahoo.com>
2021-07-07 15:55:32 +0200

    implemented text file handler

--- my_logger/handlers/file_handler.py
+++ my_logger/handlers/file_handler.py
@@ -1,14 +1,64 @@
+from datetime import datetime
 from .base_handler import Handler
 from ..log_entry import LogEntry
 from typing import List
+import os
+import shlex
+import re
+
+
+class BadHandlerFileFormatException(Exception):
+    ...
 
 
 class FileHandler(Handler):
     def __init__(self, filename: str) -> None:
-        ...
+        self.__filename = filename
+        self.__re_expr = re.compile(
+            "LogEntry\\(date=datetime.datetime\\([0-9]{4},[ 0-9,]*\\), level=.*msg=.*\\)"
+        )
 
     def add_log(self, log: LogEntry) -> None:
-        raise NotImplementedError
+
+        with open(self.__filename, "a") as text_file:
+            if os.stat(self.__filename).st_size:
+                text_file.write("\n")
+
+            text_file.write(log.__repr__())
+
+    @staticmethod
+    def __parse_datetime(ll: List[str]) -> datetime:
+        y = int(ll[0][-5:-1])
+        mth = int(ll[1][:-1])
+        d = int(ll[2][:-1])
+        h = int(ll[3][:-1])
+        m = int(ll[4][:-1])
+        s = int(ll[5][:-1])
+        ms = int(ll[6][:-2])
+        return datetime(y, mth, d, h, m, s, ms)
+
+    @staticmethod
+    def __parse_level(ll: str) -> str:
+        return ll[6:-1]
+
+    @staticmethod
+    def __parse_msg(ll: str) -> str:
+        return ll[4:-1]
 
     def get_base_form(self) -> List[LogEntry]:
-        raise NotImplementedError
+        log_entries = []
+
+        with open(self.__filename, "r") as text_file:
+            for line in text_file.readlines():
+                if not self.__re_expr.match(line):
+                    raise BadHandlerFileFormatException(
+                        f"There has been some issues with line: {line}"
+                    )
+
+                tokenized_line = shlex.split(line)
+                date = FileHandler.__parse_datetime(tokenized_line[:7])
+                level = FileHandler.__parse_level(tokenized_line[7])
+                msg = FileHandler.__parse_msg(tokenized_line[8])
+                log_entries.append(LogEntry(date=date, level=level, msg=msg))
+
+        return log_entries
---
+++ tests/test_handlers/test_file_handler.py
@@ -0,0 +1,83 @@
+from my_logger.handlers.file_handler import BadHandlerFileFormatException
+from my_logger.handlers.base_handler import Handler
+from my_logger import FileHandler, ProfilLogger, LogEntry
+import unittest
+import os
+from datetime import datetime
+
+
+# CORRECT_TEXT = """
+# LogEntry(date=datetime.datetime(2021, 7, 7, 15, 41, 4, 64954), level='INFO', msg='this is text info')
+# LogEntry(date=datetime.datetime(2021, 7, 7, 15, 41, 4, 65011), level='WARNING', msg='this is text warning')
+# LogEntry(date=datetime.datetime(2021, 7, 7, 15, 41, 4, 65045), level='ERROR', msg='this is text error')
+# """
+
+CORRECT_TEXT = (
+    "LogEntry(date=datetime.datetime(2021, 7, 7, 15, 41, 4, 64954), "
+    "level='INFO', msg='this is text info')\n"
+    "LogEntry(date=datetime.datetime(2021, 7, 7, 15, 41, 4, 65011), "
+    "level='WARNING', msg='this is text warning')\n"
+    "LogEntry(date=datetime.datetime(2021, 7, 7, 15, 41, 4, 65045), "
+    "level='ERROR', msg='this is text error')\n"
+)
+
+INVALID_TEXT = """
+jndsnd sn dnsa jkdwjkq dme mnd mnsa d
+"""
+
+TEXT_FILENAME = "test.txt"
+
+
+class TestFileHandler(unittest.TestCase):
+    def setUp(self) -> None:
+        if os.path.exists(TEXT_FILENAME):
+            os.remove(TEXT_FILENAME)
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        if os.path.exists(TEXT_FILENAME):
+            os.remove(TEXT_FILENAME)
+        return super().tearDown()
+
+    def test_handler_basic_usage(self):
+        plogger = ProfilLogger([FileHandler(TEXT_FILENAME)])
+
+        plogger.info("this is text info")
+        plogger.warning("this is text warning")
+        plogger.error("this is text error")
+
+    def test_handler_bad_file(self):
+        file_handler = FileHandler(TEXT_FILENAME)
+
+        with open(TEXT_FILENAME, "w") as txt_file:
+            txt_file.write(INVALID_TEXT)
+        self.assertRaises(
+            BadHandlerFileFormatException, file_handler.get_base_form
+        )
+
+    def test_handler_add(self):
+        file_handler = FileHandler(TEXT_FILENAME)
+
+        ref_list = [
+            LogEntry(
+                date=datetime(2021, 7, 6, 21, 53, 16, 837733),
+                level="INFO",
+                msg="this is text test file number 1",
+            ),
+            LogEntry(
+                date=datetime(2021, 7, 6, 21, 53, 16, 837885),
+                level="WARNING",
+                msg="this is text test file number 2",
+            ),
+            LogEntry(
+                date=datetime(2021, 1, 6, 22, 53, 16, 837885),
+                level="ERROR",
+                msg="this is text test error log",
+            ),
+        ]
+
+        for log in ref_list:
+            file_handler.add_log(log)
+
+        base_form = file_handler.get_base_form()
+        self.assertEqual(ref_list, base_form)
